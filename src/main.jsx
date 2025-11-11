a3c9e6f774ea670060d9173f8f21f86054963986
alihamza-12 <alihamzaarshad12@gmail.com>
2025-11-11 16:57:12 +0500

    API  call Using ReduxStore(main.jsx) provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,24 @@
 // import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {ApiProvider} from '@reduxjs/toolkit/query/react'
-import { api } from './RTKQuery/apiSlice.js'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ApiProvider } from "@reduxjs/toolkit/query/react";
+import { api } from "./RTKQuery/apiSlice.js";
+import { Provider } from "react-redux";
+import { store } from "./RTKQuery/store.js";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   // <StrictMode>
-    <ApiProvider api={api}>
-    <App />
-    </ApiProvider>
+
+  // Using the Api direct
+  // <ApiProvider api={api}>
+  //   <App />
+  // </ApiProvider>
+
+  
+  // Using the Redux Store
+  <Provider store={store}>
+     <App />
+   </Provider>
   // </StrictMode>,
-)
+);
